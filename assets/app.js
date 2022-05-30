2fd628676ace6d80830136745069198a1fb3e7d6
evrajodygildas <evrajodygildas@gmail.com>
2022-05-30 19:36:53 +0100

    Premiere page integre

---
+++ assets/app.js
@@ -0,0 +1,57 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you import will output into a single css file (app.css in this case)
+
+import $ from 'jquery/dist/jquery.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import './plugins/bootstrap/js/popper.js'
+import 'bootstrap/dist/js/bootstrap.min.js'
+
+import './plugins/animsition/js/animsition.js'
+import './plugins/css-hamburgers/hamburgers.min.css'
+
+import './fonts/font-awesome-4.7.0/css/font-awesome.css'
+
+import './plugins/select2/select2.min.css';
+import './plugins/select2/select2.min.js';
+
+import './plugins/slick/slick.css';
+import './plugins/slick/slick.min.js'
+import './slick-custom.js';
+
+import './styles/util.css'; 
+import './styles/main.css';
+
+import './main.js';
+import './plugins/sweetalert/sweetalert.min.js';
+
+
+$(".selection-1").select2({
+       minimumResultsForSearch: 20,
+       dropdownParent: $('#dropDownSelect1')
+});
+
+
+$('.block2-btn-addcart').each(function(){
+       var nameProduct = $(this).parent().parent().parent().find('.block2-name').html();
+       $(this).on('click', function(){
+              swal(nameProduct, "is added to cart !", "success");
+       });
+});
+
+$('.block2-btn-addwishlist').each(function(){
+       var nameProduct = $(this).parent().parent().parent().find('.block2-name').html();
+       $(this).on('click', function(){
+              swal(nameProduct, "is added to wishlist !", "success");
+       });
+});
+
+
+// start the Stimulus application
+// import './bootstrap';
